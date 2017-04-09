fc2ea08f83e50885f8a225a3db7924ce29be15bd
Sharo-0n <sharoon@ucsd.edu>
2017-04-08 21:05:21 -0700

    implementing another camera meathod

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ function handleVideo(stream) {
 function videoError(e) {
     // do something
 }
+
+// Grab elements, create settings, etc.
+var v = document.getElementById('video');
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ v: true }).then(function(stream) {
+        v.src = window.URL.createObjectURL(stream);
+        v.play();
+    });
+}
+// Elements for taking the snapshot
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function() {
+    context.drawImage(video, 0, 0, 640, 480);
+});
