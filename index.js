b3dbef47fd498a54ef196d3e68ffcc11fd3b7868
Sharo-0n <sharoon@ucsd.edu>
2017-04-08 21:40:22 -0700

    debugging vid

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-var video = document.querySelector("#videoElement");
- 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
- 
-if (navigator.getUserMedia) {       
-    navigator.getUserMedia({video: true}, handleVideo, videoError);
-}
- 
-function handleVideo(stream) {
-    video.src = window.URL.createObjectURL(stream);
-}
- 
-function videoError(e) {
-    // do something
-}
-
 // Grab elements, create settings, etc.
-var v = document.getElementById('video');
+var video = document.getElementById('video');
 
 // Get access to the camera!
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ v: true }).then(function(stream) {
-        v.src = window.URL.createObjectURL(stream);
-        v.play();
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
     });
 }
 // Elements for taking the snapshot
